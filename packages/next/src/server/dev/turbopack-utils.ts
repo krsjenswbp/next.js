eb1b71ca4cb0e4825540478836628132c34bdb09
Hendrik Liebau <mail@hendrik-liebau.de>
2025-08-19 19:48:51 +0200

    Do not require a request ID for the WebSocket

--- packages/next/src/server/dev/turbopack-utils.ts
+++ packages/next/src/server/dev/turbopack-utils.ts
@@ -564,7 +564,7 @@ type HandleEntrypointsHooks = {
 type HandleEntrypointsDevOpts = {
   assetMapper: AssetMapper
   changeSubscriptions: ChangeSubscriptions
-  clientsByRequestId: Map<string, ws>
+  clients: Set<ws>
   clientStates: ClientStateMap
   serverFields: ServerFields
 
@@ -798,7 +798,7 @@ async function handleEntrypointsDevCleanup({
 
   assetMapper,
   changeSubscriptions,
-  clientsByRequestId,
+  clients,
   clientStates,
 
   hooks,
@@ -826,7 +826,7 @@ async function handleEntrypointsDevCleanup({
     }
   }
 
-  for (const client of clientsByRequestId.values()) {
+  for (const client of clients) {
     const state = clientStates.get(client)
     if (!state) {
       continue
